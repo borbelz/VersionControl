f3a786c15283a6cf44eb34b7da02e77885e35611
borbelz <borzoli05@gmail.com>
2022-09-30 19:51:15 +0200

    New look

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -18,21 +18,25 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.FirstName;
-            label2.Text = Resource1.LastName;
+            label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
-            listBox1.DisplayMember = "Name";
+            listBox1.DisplayMember = "FullName";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
+
             };
             users.Add(u);
         }
