463770f35c1f0bdc4ebb09d54c0e90e7321c91c8
zoltanborbely <zoltan.borbely@stud.uni-corvinus.hu>
2022-10-21 10:45:02 +0200

    service reference

--- 5gyak_GJYYX0/5gyak_GJYYX0/Form1.cs
+++ 5gyak_GJYYX0/5gyak_GJYYX0/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5gyak_GJYYX0.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,27 @@ namespace _5gyak_GJYYX0
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            EURExchangeRates();
+        }
+
+        public void EURExchangeRates()
+        {
+           var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
