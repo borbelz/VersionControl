60027a7c51f137b67237c1f8f6f5729430cdb935
zoltanborbely <zoltan.borbely@stud.uni-corvinus.hu>
2022-10-21 11:14:40 +0200

    make diagram

--- 5gyak_GJYYX0/5gyak_GJYYX0/Form1.cs
+++ 5gyak_GJYYX0/5gyak_GJYYX0/Form1.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace _5gyak_GJYYX0
 {
@@ -19,6 +21,7 @@ namespace _5gyak_GJYYX0
         {
             InitializeComponent();
             EURExchangeRates();
+            GetXml();
 
             dgv1.DataSource = Rates;
         }
@@ -37,6 +40,46 @@ namespace _5gyak_GJYYX0
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+        }
+
+        public void GetXml()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0) rate.Value = value / unit;
+            }
+        }
+
+        public void MakeDiagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Data";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
