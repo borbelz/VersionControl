5839ec8f669475bcbbac8fedfb188ec4d00a7e19
zoltanborbely <zoltan.borbely@stud.uni-corvinus.hu>
2022-11-04 10:24:26 +0100

    Ball class

--- 6_gyak_GJYYX0/6_gyak_GJYYX0/Form1.cs
+++ 6_gyak_GJYYX0/6_gyak_GJYYX0/Form1.cs
@@ -12,6 +12,34 @@ namespace _6_gyak_GJYYX0
 {
     public partial class Form1 : Form
     {
+        public class Ball : Label
+        {
+            public Ball()
+            {
+                AutoSize = false;
+                Width = 50;
+                Height = Width;
+                Paint += Ball_Paint;
+            }
+
+            public void Ball_Paint(object sender, PaintEventArgs e)
+            {
+                DrawImage(e.Graphics);
+            }
+
+            protected void DrawImage(Graphics g)
+            {
+                g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            }
+
+
+            public void MoveBall()
+            {
+                Left += 1;
+            }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
