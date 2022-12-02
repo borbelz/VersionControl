ca1a8abd41144e38c5f82e0a99845bdb1493a357
borbelz <borzoli05@gmail.com>
2022-12-02 10:56:18 +0100

    making simulation

--- Mikroszimulacio/Mikroszimulacio/Entities/DeathProbability.cs
+++ Mikroszimulacio/Mikroszimulacio/Entities/DeathProbability.cs
@@ -8,7 +8,7 @@ namespace Mikroszimulacio.Entities
 {
     public class DeathProbability
     {
-        public string Sex { get; set; }
+        public Gender Gender { get; set; }
         public int Age { get; set; }
         public double Probability { get; set; }
     }
--- Mikroszimulacio/Mikroszimulacio/Form1.cs
+++ Mikroszimulacio/Mikroszimulacio/Form1.cs
@@ -18,9 +18,64 @@ namespace Mikroszimulacio
         List<BirthProbability> BirthProbability = new List<BirthProbability>();
         List<DeathProbability> DeathProbability = new List<DeathProbability>();
 
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
+
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbability = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbability = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep(year);
+                }
+
+                int nbrOfMales = (from x in Population
+                                    where x.Gender == Gender.Male && x.IsAlive
+                                    select x).Count();
+
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbability
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.Probability).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if(person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbability
+                                 where x.Age == age
+                                 select x.Probability).FirstOrDefault();
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person newborn = new Person();
+                    newborn.BirthYear = year;
+                    newborn.NbrOfChildren = 0;
+                    newborn.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(newborn);
+                }
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -76,7 +131,7 @@ namespace Mikroszimulacio
                     var line = sr.ReadLine().Split(';');
                     dprobability.Add(new DeathProbability()
                     {
-                        Sex = line[0],
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
                         Age = int.Parse(line[1])
                     });
                 }
