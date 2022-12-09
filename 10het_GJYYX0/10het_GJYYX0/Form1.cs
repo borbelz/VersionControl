573186ea381d66d0a7e57d1ec495eb484c74b348
borbelz <borzoli05@gmail.com>
2022-12-09 10:53:04 +0100

    final edits

--- 10het_GJYYX0/10het_GJYYX0/Form1.cs
+++ 10het_GJYYX0/10het_GJYYX0/Form1.cs
@@ -16,6 +16,12 @@ namespace _10het_GJYYX0
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+        
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +29,75 @@ namespace _10het_GJYYX0
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
+            //játék kipróbálása:
+            //gc.AddPlayer();
+            //gc.Start(true);
+
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+                
+            }
+            gc.Start();
+
+            
+
+        }
+
+        Brain winnerBrain = null;
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+            label1.BringToFront();
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            gc.Start();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
             gc.AddPlayer();
+            ga.Focus();
             gc.Start(true);
         }
     }
