0d1b58a5af7ca47851016c46c9dd44b263f9eb12
borbelz <borzoli05@gmail.com>
2022-12-09 10:22:20 +0100

    trying game

--- 10het_GJYYX0/10het_GJYYX0/Form1.cs
+++ 10het_GJYYX0/10het_GJYYX0/Form1.cs
@@ -22,6 +22,9 @@ namespace _10het_GJYYX0
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
